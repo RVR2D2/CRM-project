52f4529030b5987d471d2296a5159f6252489d50
Vadim <poompeii01@gmail.com>
2021-04-09 00:27:52 +0300

    feat: home page component many add statistic KZT,USD,EUR  real time

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import dateFilter from './filters/date.filter'
+import currencyFilter from './filters/currency.filter'
 import messagePlugin from './utils/message.plugin'
+import Loader from "./components/app/Loader";
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
@@ -13,9 +15,11 @@ import 'firebase/auth'
 import 'firebase/database'
 
 Vue.config.productionTip = false
-Vue.filter('date', dateFilter)
 Vue.use(messagePlugin)
 Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
+Vue.filter('currency', currencyFilter)
+Vue.component('Loader', Loader)
 
 
 firebase.initializeApp({
